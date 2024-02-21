12092be7c0ffb16d606e801604a717d42d2f4482
Winnie <76840294+KhinNyeinChanSoe-kncs@users.noreply.github.com>
2024-02-21 23:10:01 +0630

    remove faculty_id from user table

--- backend/group_2_ewsd_backend/app/Http/Controllers/ContributionController.php
+++ backend/group_2_ewsd_backend/app/Http/Controllers/ContributionController.php
@@ -8,6 +8,7 @@ use App\Models\Contribution;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 
 class ContributionController extends Controller
 {
@@ -24,7 +25,7 @@ class ContributionController extends Controller
         $request->validate([
             'name' => 'required',
             'description' => 'required',
-            'files' => 'required|mimes:doc,docx,pdf',
+            'files' => 'required|mimes:doc,docx',
             'images' => 'nullable|max:5',
             // |mimes:jpg,jpeg,png
             'closure_id' => 'required|exists:closures,id',
@@ -62,7 +63,6 @@ class ContributionController extends Controller
             'files' => implode(',', $uploadedFiles),
             'submitted_date' => Carbon::now()->format('Y-m-d'),
             'attempt_number' => 1,
-            'status' => 'upload',
             'is_commented' => 0,
             'closure_id' => $request->closure_id,
             'user_id' => Auth::user()->id
@@ -80,5 +80,35 @@ class ContributionController extends Controller
 
     public function update(Request $request, $id)
     {
+    }
+
+    //this function is used to approve or reject a contribution by marketing coordinator
+    public function changeStatus(Request $request,$id){
+        //need to ask date
+        $request->validate([
+            'status' => 'required',
+            'closure_id' => 'required|exists:closures,id'
+        ]);
+        //check the contribution exists or not
+        $contribution = Contribution::findOrFail($id);
+        //check the closure exists or not
+        $closure = Closure::find($request->closure_id);
+        //check the final closure date is valid or not
+        if (Carbon::parse($closure->final_closure_date)->isPast()) {
+            return $this->sendError('The closure is expired', 400);
+        }
+        // $student_faculty_id = Contribution::with(['user.facultyUsers' =>function($q){
+        //     $q->pluch('faculty_id');
+        // }])->where('id',$id)->pluck('id');
+        // return response()->json($student_faculty_id);
+        // $checkCoordinat
+        // DO WE NEED TO CHECK COORDIANTOR FACULTY ID BEFORE UPDATING THE STATUS????????
+        // if ($student_faculty_id) {
+        //     dd("here");
+        // }
+    }
+
+    public function downloadContribution(){
+
     }
 }
