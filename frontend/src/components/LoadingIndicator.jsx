616f019095da7526744c9c628417a193857c8be9
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2024-02-25 18:26:22 +0630

    [Fix]: student account creation error

--- frontend/src/components/LoadingIndicator.jsx
+++ frontend/src/components/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import "../style/tailwind.css"
 const LoadingIndicator = ({color, className, ...props}) => {
     let strokeColor = color ? color : "black"
     return (
-        <div className={`${className} animate-spin w-[24px] h-[24px] flex items-center justify-center`}>
+        <div className={`${className} animate-spin w-[24px] h-[24px] flex items-center justify-center pointer-events-none`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22 12C22 9.68645 21.1978 7.44447 19.7301 5.65607C18.2624 3.86767 16.22 2.6435 13.9509 2.19215C11.6818 1.7408 9.3264 2.09019 7.28603 3.18079C5.24566 4.27139 3.64656 6.03572 2.7612 8.17316C1.87585 10.3106 1.75901 12.6889 2.4306 14.9028C3.10219 17.1168 4.52065 19.0294 6.4443 20.3147C8.36795 21.6 10.6778 22.1786 12.9802 21.9518C15.2826 21.7251 17.4351 20.707 19.0711 19.0711" stroke={strokeColor} strokeWidth="2" strokeLinecap="round"/>
             </svg>
--- frontend/src/pages/StudentRegistrationPage.jsx
+++ frontend/src/pages/StudentRegistrationPage.jsx
@@ -3,6 +3,7 @@ import Breadcrumb from "../components/Breadcrumb"
 import InputField from "../components/InputField"
 import Dropdown from "../components/Dropdown"
 import { useState } from "react"
+import LoadingIndicator from "../components/LoadingIndicator"
 
 const StudentRegistrationPage = () => {
 
@@ -14,6 +15,23 @@ const StudentRegistrationPage = () => {
     let [password, setPassword] = useState("");
     let [retype, setRetype] = useState("");
     let [faculty, setFaculty] = useState(null);
+
+    let [error, setError] = useState(null);
+    let [isLoading, setIsLoading] = useState(false);
+
+    async function createAccount() {
+        setError(null)
+        if (!username) { setError("Username must not be empty") ; return }
+        if (!email) { setError("Email must not be empty") ; return }
+
+        if (!faculty) { setError("Faculty is not selected"); return }
+
+        if (!password) { setError("Password must not be empty") ; return }
+        if (!retype || password !== retype) { setError("Password must be the same") ; return }
+
+        setIsLoading(true)
+        // setIsLoading(false)
+    }
 
     return (
         <div className="flex flex-col gap-4 w-full p-4 px-8">
@@ -49,10 +67,22 @@ const StudentRegistrationPage = () => {
                     <InputField className="grow" placeholder="password" type="password" value={password} onChange={setPassword}/>
                     <InputField className="grow" placeholder="retype password" type="password" value={retype} onChange={setRetype}/>
                 </div>
+                {error && <p className="w-full p-2 text-center rounded border border-red-100 bg-red-50 font-serif text-sm text-red-500">
+                    {error}
+                </p>}
                 <div className="flex w-full gap-4 md:grap-8 md:w-[300px] md:mx-auto">
-                    <button className="grow basis-0 p-2 px-4 rounded bg-purple-500 text-white hover:opacity-50 transition-all" onClick>Save</button>
-                    <button className="grow basis-0 p-2 px-4 rounded bg-gray-400 text-white hover:opacity-50 transition-all" onClick>Cancel</button>
+                    <button 
+                        className={`${isLoading && 'hidden'} grow basis-0 p-2 px-4 rounded bg-purple-500 text-white hover:opacity-50 transition-all`} 
+                        onClick={createAccount}
+                    >Save</button>
+                    <Link 
+                        className={`${isLoading && 'hidden'} grow basis-0 p-2 px-4 rounded bg-gray-400 text-white text-center hover:opacity-50 transition-all`} 
+                        to="/users">
+                        Cancel
+                    </Link>
+                    {isLoading && <div className="flex items-center justify-center w-full"><LoadingIndicator/></div>}
                 </div>
+                    
             </div>
         </div>
     )
