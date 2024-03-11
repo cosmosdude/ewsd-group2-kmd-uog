bc4260bc5b7646edb0ffab7d9d79a2e9ca08dc69
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2024-03-11 18:25:20 +0630

    Make command runnable

--- setup-backend.sh
+++ setup-backend.sh
@@ -1,6 +1,13 @@
 cd 'backend/group_2_ewsd_backend'
+
+echo "Preparing Environment File ..."
+
 cp '.env.example' '.env'
 sed -i '' "s/DB_DATABASE=laravel/DB_DATABASE=uog_kmd_comp_1640_ewsd_group_2/g" .env
+echo ""
+
+echo "Installing vendor files ..."
+sleep 0.1
 composer install
 php artisan migrate
 php artisan passport:install
