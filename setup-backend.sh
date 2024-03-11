fa0b28693f442a0bb0f4837af36388ed7e209477
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2024-03-11 17:56:42 +0630

    Updated server setup script

--- setup-backend.sh
+++ setup-backend.sh
@@ -1,2 +1,8 @@
+cd 'backend/group_2_ewsd_backend'
+cp '.env.example' '.env'
+sed -i '' "s/DB_DATABASE=laravel/DB_DATABASE=uog_kmd_comp_1640_ewsd_group_2/g" .env
+composer install
+php artisan migrate
 php artisan passport:install
 php artisan key:generate
+php artisan db:seed
